798abe920c5a19c40408a1fc628dfbd55cdf2b2c
Slawomir-Ickiewicz <s.ickiewicz@samsung.com>
2019-05-24 10:56:59 +0200

    Working solution of ex 4.39 - code can be refactored

--- src/Chapter04/EX_4_39_PG_226/Crypto.java
+++ src/Chapter04/EX_4_39_PG_226/Crypto.java
@@ -15,51 +15,71 @@ import java.util.Scanner;
 public class Crypto {
 
 
-
     public static void main(String[] args) {
 
 
-            Scanner in = new Scanner(System.in);
-            System.out.print("Input four non-negative digits: ");
-            int input = in.nextInt();
-            int n1Before = input / 1000 % 10;
-            int n2Before = input / 100 % 10;
-            int n3Before = input / 10 % 10;
-            int n4Before = input % 10;
-            System.out.println("Original numbers are "+n1Before + " " + n2Before + " " + n3Before + " " + n4Before);
-            int n1AfSum = (input / 1000 % 10)+7;
-            int n2AfSum = (input / 100 % 10)+7;
-            int n3AfSum = (input / 10 % 10)+7;
-            int n4AfSum= (input % 10)+7;
-            System.out.println("After add 7 to all digits "+n1AfSum + " " + n2AfSum + " " + n3AfSum + " " + n4AfSum);
-            int n1AfMod = ((input / 1000 % 10)+7)%10;
-            int n2AfMod = ((input / 100 % 10)+7)%10;
-            int n3AfMod = ((input / 10 % 10)+7)%10;
-            int n4AfMod= ((input % 10)+7)%10;
-            System.out.println("After mod10 for all digits "+n1AfMod + " " + n2AfMod + " " + n3AfMod + " " + n4AfMod);
-            int x = n1AfMod;
-            int y = n2AfMod;
-            n2AfMod = n4AfMod;
-            n4AfMod = y;
-            n1AfMod = n3AfMod;
-            n3AfMod = x;
-            System.out.println("After replace 1st<->3rd and 2nd<->4th "+n1AfMod+ n2AfMod+ n3AfMod+ n4AfMod);
-            String userString = "" + n1AfMod+n2AfMod+n3AfMod+n4AfMod;
-            System.out.println(userString);
-      String x2 = String.format("%04d", Integer.parseInt(userString));
-       int x3 = Integer.parseInt(String.valueOf(userString));
-        System.out.println(x3);
+        Scanner in = new Scanner(System.in);
 
 
+        System.out.print("Input four non-negative digits fo encryption: ");
+        int input = in.nextInt();
+        int n1Before = input / 1000 % 10;
+        int n2Before = input / 100 % 10;
+        int n3Before = input / 10 % 10;
+        int n4Before = input % 10;
+        System.out.println("Original numbers are " + n1Before + " " + n2Before + " " + n3Before + " " + n4Before);
+        int n1AfSum = (input / 1000 % 10) + 7;
+        int n2AfSum = (input / 100 % 10) + 7;
+        int n3AfSum = (input / 10 % 10) + 7;
+        int n4AfSum = (input % 10) + 7;
+        System.out.println("After add 7 to all digits " + n1AfSum + " " + n2AfSum + " " + n3AfSum + " " + n4AfSum);
+        int n1AfMod = ((input / 1000 % 10) + 7) % 10;
+        int n2AfMod = ((input / 100 % 10) + 7) % 10;
+        int n3AfMod = ((input / 10 % 10) + 7) % 10;
+        int n4AfMod = ((input % 10) + 7) % 10;
+        System.out.println("After mod10 for all digits " + n1AfMod + " " + n2AfMod + " " + n3AfMod + " " + n4AfMod);
+        int x = n1AfMod;
+        int y = n2AfMod;
+        n2AfMod = n4AfMod;
+        n4AfMod = y;
+        n1AfMod = n3AfMod;
+        n3AfMod = x;
+        System.out.println("After replace 1st<->3rd and 2nd<->4th " + n1AfMod + n2AfMod + n3AfMod + n4AfMod);
+        System.out.println();
 
+        System.out.print("Input four non-negative digits for decryption: ");
+        int inputDec = in.nextInt();
+        int n1BeforeDec = inputDec / 1000 % 10;
+        int n2BeforeDec = inputDec / 100 % 10;
+        int n3BeforeDec = inputDec / 10 % 10;
+        int n4BeforeDec = inputDec % 10;
+        System.out.println("Original numbers for decryption are " + n1BeforeDec + " " + n2BeforeDec + " " + n3BeforeDec + " " + n4BeforeDec);
 
+        int a = n1BeforeDec;
+        n1BeforeDec = n3BeforeDec;
+        n3BeforeDec = a;
 
+        int b = n2BeforeDec;
+        n2BeforeDec = n4BeforeDec;
+        n4BeforeDec = b;
+        System.out.println("Nums for decryption after replace 1st<->3rd and 2nd<->4th " + n1BeforeDec + n2BeforeDec + n3BeforeDec + n4BeforeDec);
 
-
+        n1BeforeDec = decrypt(n1BeforeDec);
+        n2BeforeDec = decrypt(n2BeforeDec);
+        n3BeforeDec = decrypt(n3BeforeDec);
+        n4BeforeDec = decrypt(n4BeforeDec);
+        System.out.println("Number after decryption are " + n1BeforeDec + " " + n2BeforeDec + " " + n3BeforeDec + " " + n4BeforeDec);
 
     }
 
+    private static int decrypt(int number) {
 
-
+        if (number >= 7) {
+            number -= 7;
+        } else {
+            number += 3;
+        }
+        return number;
+    }
 
 }
